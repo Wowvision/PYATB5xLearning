8506c9d54669dea29d4eda3eeaa25cbc514c5cbd
varti <shubhamynrcs@gmail.com>
2024-11-08 20:07:41 +0530

    Lab003 added

---
+++ NOV/ex_05112014_Python_Basics/Lab003_python_Basics.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+print("Shubham Goel")
+
+
+# print(self, *args, sep=' ', end='\n', file=None):
+# print(, *args, sep=' ', end='\n', file=None):
+# *args - unlimited number of comma seperated arguments
+#     print("Pramod Dutta",123,"Amit","John") # IndentationError: unexpected indent
+
+
+print("Shubham Goel", 123, 456, "Amit", "john")
+print("Shubham Goel", 123, 456, "Amit", "john", sep='_',end=" ")
+print("Shubham Goel", 123, 456, "Amit", "john", sep='*')
